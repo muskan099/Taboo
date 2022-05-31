d725d8f868fc132e89a4beb16683504bb8f48f45
RyQuest <rajkumaryadav@questglt.com>
2022-05-31 13:11:34 +0530

    fvdfvdg

--- src/pages/CreateNft.js
+++ src/pages/CreateNft.js
@@ -453,7 +453,7 @@ const CreateNft = () => {
                               <option>Selct Category</option>
                               <option value="Age">Age</option>
                               <option value="Nude">Nude</option>
-                              <Option value="Khloe Terae">Khloe Terae</Option>
+                              <option value="Khloe Terae">Khloe Terae</option>
                               <option value="Jeni Summers">Jeni Summers</option>
                               <option value="Sexy">Sexy</option>
                               <option value="Metaverse">Metaverse</option>
