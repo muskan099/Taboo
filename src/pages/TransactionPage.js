a1d88cc2195e4e692c63d4360578cbdcaa480292
RyQuest <rajkumaryadav@questglt.com>
2022-07-22 18:01:52 +0530

    vrsgbrg

--- src/pages/TransactionPage.js
+++ src/pages/TransactionPage.js
@@ -315,9 +315,9 @@ const TransactionPage = () => {
                             <td>
                            
 
-                                         <button disabled={item.contentinfo.status=="sold"?false:true} onClick={()=>handleCreateSale(item)}>{item.isOwner=="no"?"sold":"Sell"}</button>
+                                         <button disabled={item.contentinfo.status=="sold"||item.isOwner=="yes"?false:true} onClick={()=>handleCreateSale(item)}>{item.isOwner=="no"?"sold":"Sell"}</button>
  
-                                          <button disabled={item.contentinfo.status=="active"?false:true} onClick={()=>handleWithdrawSale(item.contentinfo)}>Cancel</button>
+                                         <button disabled={item.contentinfo.status=="active"&&item.isOwner=="yes"?false:true} onClick={()=>handleWithdrawSale(item.contentinfo)}>Cancel</button>
 
                                        {/*
 
